603bc102f76371ce2f08eb8a3e93d88b41f114c1
joshuamiguel18 <85439959+joshuamiguel18@users.noreply.github.com>
2022-07-12 09:45:02 +0800

    update 1
    improved animation, improved about, hero and skills section. Added Projects section

--- src/script.js
+++ src/script.js
@@ -1,29 +1,54 @@
-
-function copyEmail() {
+/*function copyEmail() {
     navigator.clipboard.writeText("joshuamigueltoribio18@gmail.com")
     document.querySelector('h3').style.opacity = '1';
     setTimeout(() => {
-        document.querySelector('h3').style.opacity = '0';},500);
-}
+        
+}*/
 
+var imgs = document.getElementsByTagName("img");
 setInterval(() => {
-    if(window.scrollY > 100) {
-        document.querySelector('.skills .container').style.opacity = 1;
-        document.querySelector('.skill').style.opacity = 1;
-        document.querySelector('.skill').style.left = "45%";
+    if(window.scrollY > 200) {
 
+        document.querySelector('.skill').style.opacity = 1;
+        document.querySelector('.skill').style.left = "20%";
 
     }else {
         document.querySelector('.skill').style.left = "80%";
-        document.querySelector('.skills .container').style.opacity = 0;
+        document.querySelector('.skill').style.opacity = 0;
     }
     console.log(window.scrollY)
 
 },200);
+var services = document.getElementsByClassName('service-hub');
+setInterval(() => {
+    if(window.scrollY > 400) {
+        document.getElementById('techs').style.left = "15%";
+        document.getElementById('techs').style.opacity = 1;
+        for(service of services) { 
+            service.style.transform = "scale(1)"
+            service.style.opacity = 1;
+        }
+        for(img of imgs) {
+            img.style.transform = "rotate(360deg)"
+        }
 
+    }else {
+        document.getElementById('techs').style.left = "-60%";
+        for(service of services) { 
+            service.style.transform = "scale(.5)"
+            service.style.opacity = 0;
+        }
+        for(img of imgs) {
+            
+            img.style.transform= "rotate(0deg)";
+        }
+    }
+    console.log(window.scrollY)
+
+},200);
 setInterval(() => {
     if(window.scrollY > 800) {
-        document.querySelector('.about .container').style.opacity = 1;
+        document.querySelector('.about .container').style.opacity = window.scrollY * .001;
 
     }else {
         document.querySelector('.about .container').style.opacity = 0;
@@ -31,3 +56,9 @@ setInterval(() => {
     console.log(window.scrollY)
 
 },200);
+
+function move() {
+    alert("Mssage");
+    window.scrollTo(0,2006);
+}
+
